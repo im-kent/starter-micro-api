5020fd7d05bb19a44c36853b3fcf3c07160cacaa
im-kent <116394058+im-kent@users.noreply.github.com>
2023-11-09 21:33:40 -0800

    Use express

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express'); // Include the express module
+const app = express(); // Create an instance of express
+
+// Define a route for GET requests on the root URL '/'
+app.get('/', (req, res) => {
+  // Respond with a JSON object when the root URL is accessed
+  res.json({ message: 'Hello, World!' });
+});
+
+// Start the server on port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port ' + process.env.PORT || 3000);
+});
